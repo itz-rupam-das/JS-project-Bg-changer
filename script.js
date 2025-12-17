ce5ea0c18e1a76a5eb283a1159a2c68cb87e73c5
Rupam_Das <rdas40978@gmail.com>
2025-12-17 23:18:02 +0530

    finish the remaining js part and css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btn = document.querySelectorAll(".button");
 const screen = document.querySelector("body");
 const container = document.querySelector(".container")
+const buttons = document.querySelector(".buttons")
 
 btn.forEach((button) =>{
   button.addEventListener('click',(e) =>{
@@ -8,6 +9,22 @@ btn.forEach((button) =>{
     if(e.target.id === "red"){
       screen.style.backgroundColor = e.target.id;
       container.style.backgroundColor = e.target.id;
+      buttons.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === "blue"){
+      screen.style.backgroundColor = e.target.id;
+      container.style.backgroundColor = e.target.id;
+      buttons.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === "green"){
+      screen.style.backgroundColor = e.target.id;
+      container.style.backgroundColor = e.target.id;
+      buttons.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === "yellow"){
+      screen.style.backgroundColor = "rgb(242, 185, 0)";
+      container.style.backgroundColor = "rgb(242, 185, 0)";
+      buttons.style.backgroundColor = "rgb(242, 185, 0)";
     }
   })
 })
